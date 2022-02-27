1a0081c2b3584047f071c6ccfce066e7ae0d6fdb
ramesh bandgar <ramesh.bandgar@gmail.com>
2022-02-27 18:41:40 +0530

    css added

--- dothraki_api/dothraki.js
+++ dothraki_api/dothraki.js
@@ -1,35 +1,20 @@
-// var btnTranslate=document.querySelector("#btn-translate")
 
-// btnTranslate.addEventListener("click", function clickEventHandler()
-// {
-//     console.log("clicked")
-// })
-
-
-// This is another way to handle the event
-/*
-var btnTranslate=document.querySelector("#btn-translate")
-
-function clickEventHandler(){
-    console.log("clicked")
-}
-
-btnTranslate.addEventListener("click", clickEventHandler)
-*/
 
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
-var serverURL="https://api.funtranslations.com/translate/minion.json"
+var serverURL="https://api.funtranslations.com/translate/dothraki.json"
 function getTranslationURL(text){
     return serverURL + "?" + "text=" + text
 }
+
 
 function errorHandler(error){
     console.log("Error occured", error);
     alert("Something wrong with server! try again after some time")
 }
+
 
 function clickHandler() {
     var inputText=txtInput.value;
