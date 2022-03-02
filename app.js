0eba8a5a956ffc8557e5cc70f173fe5033795b09
ramesh bandgar <ramesh.bandgar@gmail.com>
2022-03-02 06:20:32 +0530

    update
    made some changes in  css

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-// var btnTranslate=document.querySelector("#btn-translate")
 
-// btnTranslate.addEventListener("click", function clickEventHandler()
-// {
-//     console.log("clicked")
-// })
-
-
-// This is another way to handle the event
-/*
-var btnTranslate=document.querySelector("#btn-translate")
-
-function clickEventHandler(){
-    console.log("clicked")
-}
-
-btnTranslate.addEventListener("click", clickEventHandler)
-*/
 
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
